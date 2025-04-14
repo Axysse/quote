ca7a41783ad2193b8f8abeba4defcc5e6ff6c447
unknown <nicolas.vouillerot@gmail.com>
2025-04-14 14:54:27 +0200

    polish

--- assets/js/quotes.js
+++ assets/js/quotes.js
@@ -8,6 +8,7 @@ const quotes = [
     { text : "Tu aimes les omelettes? Tiens, je te casse les oeufs!", img: "./assets/img/last.jpg" },
     { text : "Il y a 9 millions de terroristes dans le monde , j'en tue un et il a les pieds plus petit que ma soeur.", img: "./assets/img/die.jpg" },
     { text : "Petit, tu es doué, très doué ; mais tant que je serai dans le métier, tu ne seras jamais que le second.", img: "./assets/img/mask.jpg" },
+    { text : "S'améliorer soi-même c'est de la masturbation. ", img: "./assets/img/fight-club.jpg" },
 ]
 
 const displayText = document.getElementById("displayText")
